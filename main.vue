05a61a945b59cff331d3c30c839c0d0aaa005114
Justin Kimbrell <jkimbrell@actengage.com>
2020-08-28 17:17:59 -0600

    pre-release commit

--- main.vue
+++ main.vue
@@ -1,10 +1,5 @@
 <script>
 import './src/css/ListGroup.css';
 import ListGroup from './src/ListGroup';
-
-import { colorLevel } from '@vue-interface/tailwindcss';
-
-console.log(colorLevel('#4299e1', -9));
-
 export default ListGroup;
 </script>
